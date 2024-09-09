9d70d9b4b0410eaaa20520a90cba19ef1b47df64
rojasV <rojasvladimir.vr@gmail.com>
2024-09-08 22:45:51 -0300

    criando configuração para build de backend 7

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://film-makers-frontend.vercel.app', 'http://localhost:3000'],
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
 
   const configService = app.get(ConfigService);
-  console.log('JWT_SECRET:', configService.get('JWT_SECRET')); 
-  
+  console.log('JWT_SECRET:', configService.get('JWT_SECRET'));
+
   await app.listen(3001);
   console.log(`Application is running on: http://localhost:3001`);
 }
